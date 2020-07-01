26a217e9cccbd7519c05042cbfc99649c576c0fa
Si Yun (Steven Kim) <dev@donga.ac.kr>
2020-07-01 13:12:46 +0900

    #update

--- beakjoon 2020/17298.cpp
+++ beakjoon 2020/17298.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <stack>
+#include <utility>
 
 using namespace std;
+
+stack<pair<int,int>> processStack;
 
 vector<int> solution(vector<int> inputData)
 {
 	vector<int> answer;
-
 	
+	for (int i = 0; i < inputData.size(); i++)
+	{
+		if (processStack.size() == 1)
+		{
+			pair<int, int> temp = processStack.top();
+		}
+		else
+		{
 
+		}
 
-	// vector를 i부터 돌아서 ? 찾는다?
+	}
 
-	// list를 순회해서 찾는다?
-
-	// stack을 써서 이전 값 기억 후 비교 ? 이건 예외가 잇을듯?
+	// stack을 써서 이전 값 기억 후 비교 ? 이건 예외가 잇을듯? 이걸 처리해야할뜻
 
 	// 일단 맨 마지막번째 수의 오큰수는 무조건 -1일 것이다..
 
